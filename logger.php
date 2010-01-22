4ef4dc224cfbc1852be66c0b2d74f1c93aea07a7
weatheredwatcher <weatheredwatcher@gmail.com>
2010-01-22 16:47:16 -0500

    1-22-2010 final

--- logger.php
+++ logger.php
@@ -4,18 +4,25 @@
 	Thinking of showing a dump, then offering a drop-down with various pages on the log to jump straight to that page log hits
 */
 include 'includes/dbconnect.php';
-$results = mysql_query("SELECT * FROM tbl_LOG ");
+$results = mysql_query("SELECT * FROM tbl_LOG ORDER BY id DESC");
 $timestamps = mysql_query("SELECT DISTINCT(timstamp) FROM tbl_LOG");
 $page_locations = mysql_query("SELECT DISTINCT(page_location) FROM tbl_LOG");
-while($row=mysql_fetch_row($results)){
-	
-	$page_location = $row[1];
-	$timestamp = $row[2];
-}
+
 ?>
 
 <h2>System Log</h2>
 
-<table>
-	<tr><td><?=$page_location?></td><td><?=$timestamp?></td></tr>
+<div id="log">
+<table border="1">
+<tr><th>Page Location</th><th>Message</th><th>Timestamp</th></tr>	
+	<?
+	while($row=mysql_fetch_row($results)){
+
+		$page_location = $row[1];
+		$msg = $row[2];
+		$timestamp = $row[3];
+	?>
+	<tr><td><?=$page_location?></td><td><?=$msg?></td><td><?=$timestamp?></td></tr>
+	<?}?>
 	</table>
+	</div>
